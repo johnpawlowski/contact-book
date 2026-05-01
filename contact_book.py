d5346f3ce514dd1458ac151ac03ba1ada871c940
John Pawlowski <john.pawlowski93@gmail.com>
2026-05-01 13:47:11 -0700

    refactor ContactBook.list() condition to convert the datetime object in created_at to a string

--- contact_book.py
+++ contact_book.py
@@ -39,7 +39,8 @@ class ContactBook:
     def list(self):
         print(f"{'Name':<20}{'Email':<20}{'Phone':<20}{'Created At':<20}")
         for contact in self._contacts.values():
-            print(f"{contact.name:<20}{contact.email:<20}{contact.phone:<20}{contact.created_at:<20}")
+            created = contact.created_at.strftime("%Y-%m-%d %H:%M")
+            print(f"{contact.name:<20}{contact.email:<20}{contact.phone:<20}{created:<20}")
     
     def remove(self, email):
         if email not in self._contacts:
