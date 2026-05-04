7023733d3319048d3963139f0038979f5b2d5d7a
John Pawlowski <john.pawlowski93@gmail.com>
2026-05-04 16:13:13 -0700

    add choice to update contact to main()

--- contact_book_cli.py
+++ contact_book_cli.py
@@ -12,10 +12,11 @@ def main():
         
         print("""----- Contact Book CLI -----
     1. Add Contact
-    2. Remove Contact
-    3. Find Contact by Email
-    4. List All Contacts
-    5. Quit""")
+    2. Update Contact
+    3. Remove Contact
+    4. Find Contact by Email
+    5. List All Contacts
+    6. Quit""")
         
         choice = input("Choose a menu item number: ").strip()
 
@@ -34,9 +35,29 @@ def main():
             new_contact = Contact(name, email, address, phone)
             contacts.add(new_contact)
             contacts.save()
+        
+        if choice == "2":
+            email = input("Enter the email of the contact to update: ").strip().lower()
+            if email not in contacts.contacts:
+                print(f"{cb.ContactNotFoundError(email)}")
+                continue
+            key = input("Enter the field to update: ").strip().lower()
+            if key == "address":
+                street = input("New Address (street): ").strip()
+                city = input("New Address (city): ").strip()
+                country = input("New Address (country): ").strip()
+                new_value = Address(street, city, country)
+            else:
+                new_value = input(f"New {key}: ").strip()
+            try:
+                contacts.update(email, key, new_value)
+                contacts.save()
+                print(f"Contact updated.")
+            except ValueError as e:
+                print(f"Error: {e}")
 
         # remove contact
-        elif choice == "2":
+        elif choice == "3":
             if not contacts.contacts:
                 print("No contacts exist.")
                 continue
@@ -48,7 +69,7 @@ def main():
                 print(f"{e}")
 
         # view contact
-        elif choice == "3":
+        elif choice == "4":
             if not contacts.contacts:
                 print("No contacts exist.")
                 continue
@@ -60,19 +81,19 @@ def main():
                 print(f"{e}")
 
         # list contacts
-        elif choice == "4":
+        elif choice == "5":
             if not contacts.contacts:
                 print("No contacts exist.")
                 continue
             contacts.list()
 
         # quit program
-        elif choice == "5":
+        elif choice == "6":
             print("Goodbye")
             break
 
         else:
-            print("Invalid selection. Please choose 1-5.")
+            print("Invalid selection. Please choose 1-6.")
 
 
 if __name__ == "__main__":
